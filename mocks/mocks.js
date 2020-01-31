4a6378ed53c411ffa86404eb18fa39bf99fcad10
EduRomoGO <eduromogo@outlook.es>
2020-01-31 12:18:45 +0000

    mocks

---
+++ mocks/mocks.js
@@ -0,0 +1,18 @@
+export const descriptors = [
+  {
+      "word": "school",
+      "phonemics": "/ˈskuːl/",
+  },
+  {
+      "word": "bakery",
+      "phonemics": "/ˈbeɪkəri/",
+  },
+  {
+      "word": "bus",
+      "phonemics": "/ˈskuːl/",
+  },
+  {
+      "word": "window",
+      "phonemics": "/ˈbeɪkəri/",
+  },
+];

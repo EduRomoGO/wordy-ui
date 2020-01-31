8f676f801064c7b2b5f11e91e90d82559a8523c8
EduRomoGO <eduromogo@outlook.es>
2020-01-31 15:01:40 +0000

    fix: avoid error of playing the wrong sound due to keys mismanagament

--- src/components/Word/Word.js
+++ src/components/Word/Word.js
@@ -1,11 +1,9 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import './Word.css';
 
 const Word = ({short, word, phonemics}) => {
-    const audio = useRef(null);
-
-    const handleOnMouseOver = () => {
-        audio.current.play();
+    const handleOnMouseOver = (e) => {
+        document.querySelector(`#${word}`).play();
     };
 
     const getWord = () => short
@@ -22,8 +20,8 @@ const Word = ({short, word, phonemics}) => {
 
     return <div className={getClassName()}>
         {getWord()}
-        <audio hidden={short} ref={audio} controls>
-            <source  src={`./audioFiles/${word}.mp3`} type="audio/mpeg"></source>
+        <audio hidden={short} id={word} controls>
+            <source src={`./audioFiles/${word}.mp3`} type="audio/mpeg"></source>
             Your browser does not support the audio element.
         </audio>
     </div>
--- src/components/Words/Words.js
+++ src/components/Words/Words.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Word from '../Word/Word.js';
 import './Words.css';
-
+import cuid from 'cuid';
 
 const Words = ({words, isFilterActive}) => {
     const getClassName = () => isFilterActive ? 'short' : 'normal';
+    const getKey = id => isFilterActive ? cuid() : id;
 
     return <div className={`words-${getClassName()}`}>
-        {words.map(({word, phonemics}, id) => <Word short={isFilterActive} key={id} word={word} phonemics={phonemics} />)}
+        {words.map(({word, phonemics}, id) => <Word short={isFilterActive} key={getKey(id)} word={word} phonemics={phonemics} />)}
     </div>
 };
 

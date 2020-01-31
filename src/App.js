1dd7f465ea39b71e31dd2eb3f3fef6f04657d5d7
EduRomoGO <eduromogo@outlook.es>
2020-01-31 12:16:36 +0000

    feat: add db info to show in app

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
 import React, { useState } from 'react';
 import './App.css';
 import Words from './components/Words/Words.js';
+import db from './utils/db/db.json';
+
 
 // [ ] Crear web con todas las palabras de mi bbdd de palabras
 // [ ] Ver como hacer para que se cacheen los audios, ya que seria una web bastante pesada
 // [ ] Crear buscador que puedas introducir cualquier texto, y ordene esas palabras en los resultados de busqueda y oculte las demas palabras que no esten en el texto. Ademas cada palabra tendra a continuacion su phonetica es decir: run (phon) through (phon)... y al hacer hover se escuchara la pronunciacion de cada palabra
 
 // import { pipe, when } from 'ramda';
+// const descriptors = [
+//   {
+//       "word": "school",
+//       "phonemics": "/ˈskuːl/",
+//   },
+//   {
+//       "word": "bakery",
+//       "phonemics": "/ˈbeɪkəri/",
+//   },
+//   {
+//       "word": "bus",
+//       "phonemics": "/ˈskuːl/",
+//   },
+//   {
+//       "word": "window",
+//       "phonemics": "/ˈbeɪkəri/",
+//   },
+// ];
 
-const descriptors = [
-  {
-      "word": "school",
-      "phonemics": "/ˈskuːl/",
-  },
-  {
-      "word": "bakery",
-      "phonemics": "/ˈbeɪkəri/",
-  },
-  {
-      "word": "bus",
-      "phonemics": "/ˈskuːl/",
-  },
-  {
-      "word": "window",
-      "phonemics": "/ˈbeɪkəri/",
-  },
-];
+const descriptors = db.wordDescriptors.map(item => ({ word: item.word, phonemics: item.phonemics }));
 
 function App() {
   const [search, setSearch] = useState('');

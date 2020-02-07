8d7d665818f24ec0f5d4d66c63cec8d6151ca7f0
EduRomoGO <eduromogo@outlook.es>
2020-02-07 14:10:14 +0000

    feat: improve phonemes visualization

--- src/components/Phonemes/Phonemes.js
+++ src/components/Phonemes/Phonemes.js
@@ -1,191 +1,8 @@
 import React from 'react'
 import './Phonemes.css';
+import { consonants, vowels, diphthongs } from './PhonemesList.js';
 
 const Phonemes = () =>{
-  const consonants = [
-    {
-      phonem: 'p', 
-      word: 'soup'
-    },{
-      phonem: 'b',
-      word: 'bat'
-    },
-    {
-      phonem: 't',
-      word:'light'
-    },
-    {
-      phonem: 'd',
-      word: 'dog'
-    },
-    {
-      phonem: 'k',
-      word: 'cap'
-    },
-    {
-      phonem: 'g',
-      word: 'bag'
-    },
-    {
-      phonem: 'm',
-      word: 'man'
-    },
-    {
-      phonem: 'n',
-      word: 'rain'
-    },
-    {
-      phonem: 'ŋ',
-      word: 'king'
-    },
-    {
-      phonem: 'f',
-      word: 'fish'
-    },
-    {
-      phonem: 'v',
-      word: 'van'
-    },
-    {
-      phonem: 'θ',
-      word: 'thumb'
-    },
-    {
-      phonem: 'ð',
-      word: 'mother'
-    },
-    {
-      phonem: 's',
-      word: 'mouse'
-    },
-    {
-      phonem: 'z',
-      word: 'zebra'
-    },
-    {
-      phonem: 'ʃ',
-      word: 'shoe'
-    },
-    {
-      phonem: 'ʒ',
-      word: 'television'
-    },
-    {
-      phonem: 'h',
-      word: 'hand'
-    },
-    {
-      phonem: 'tʃ',
-      word: 'cheese'
-    },
-    {
-      phonem: 'dʒ',
-      word: 'jet'
-    },
-    {
-      phonem: 'r',
-      word: 'write'
-    },
-    {
-      phonem: 'l',
-      word: 'long'
-    },
-    {
-      phonem: 'j',
-      word: 'yo-yo'
-    },
-    {
-      phonem: 'w',
-      word: 'whale'
-    }
-  ]
-
-  const vowels = [    
-    {
-      phonem: 'i:',
-      word: 'sea'
-    },
-    {
-      phonem: 'ɪ',
-      word: 'swim'
-    },
-    {
-      phonem: 'e',
-      word: 'bed'
-    },
-    {
-      phonem: 'æ',
-      word: 'cat'
-    },
-    {
-      phonem: 'ɑ:',
-      word: 'car'
-    },
-    {
-      phonem: 'ʌ',
-      word: 'cup'
-    },
-    {
-      phonem: 'ɒ',
-      word: 'lock'
-    },
-    {
-      phonem: 'ɔ:',
-      word: 'ball'
-    },
-    {
-      phonem: 'ʊ',
-      word: 'book'
-    },
-    {
-      phonem: 'u:',
-      word: 'two'
-    },
-    {
-      phonem: 'ɜ:ʳ',
-      word: 'shirt'
-    },
-    {
-      phonem: 'ə',
-      word: 'father'
-    }
-  ]
-
-  const diphthongs = [ 
-    {
-      phonem: 'eɪ',
-      word: 'eight'
-    },
-    {
-      phonem: 'aɪ',
-      word: 'eye'
-    },
-    {
-      phonem: 'ɔɪ',
-      word: 'boy'
-    },
-    {
-      phonem: 'oʊ',
-      word: 'arrow'
-    },
-    {
-      phonem: 'aʊ',
-      word: 'house'
-    },
-    {
-      phonem: 'eəʳ',
-      word: 'chair'
-    },
-    {
-      phonem: 'ɪəʳ',
-      word: 'deer'
-    },
-    {
-      phonem: 'ʊə',
-      word: 'tourist'
-    },
-  ]
-
   const handleOnMouseOver = (e, file) => {
     document.querySelector(`#${file}`).play();
   };
@@ -194,10 +11,10 @@ const Phonemes = () =>{
     <div className="phonemSection">
       <p className="title">{type}</p>
       <div className={`phonemList ${type}`}>
-        {items.map(({ phonem, word })=> {
+        {items.map(({ phonem, word }, id)=> {
           const newPhonem = phonem.includes(':') ? phonem.replace(":", "\\:") : phonem
           return (  
-          <div className="pair">
+          <div key={id} className="pair">
             <span 
               className="phonem" 
               onMouseOver={(e)=>handleOnMouseOver(e, newPhonem)}
@@ -205,7 +22,7 @@ const Phonemes = () =>{
             >
               {phonem}
             </span>
-            <audio hidden={false} id={phonem} controls>
+            <audio hidden={true} id={phonem} controls>
               <source src={`./phonemesFiles/${phonem}.mp3`} type="audio/mpeg"></source>
               Your browser does not support the audio element.
             </audio>
@@ -216,7 +33,7 @@ const Phonemes = () =>{
             >
               {word}
             </span>
-            <audio hidden={false} id={word} controls>
+            <audio hidden={true} id={word} controls>
               <source src={`./phonemesFiles/${word}.mp3`} type="audio/mpeg"></source>
               Your browser does not support the audio element.
             </audio>
---
+++ src/components/Phonemes/PhonemesList.js
@@ -0,0 +1,183 @@
+export const consonants = [
+    {
+        phonem: 'p',
+        word: 'soup'
+    }, {
+        phonem: 'b',
+        word: 'bat'
+    },
+    {
+        phonem: 't',
+        word: 'light'
+    },
+    {
+        phonem: 'd',
+        word: 'dog'
+    },
+    {
+        phonem: 'k',
+        word: 'cap'
+    },
+    {
+        phonem: 'g',
+        word: 'bag'
+    },
+    {
+        phonem: 'm',
+        word: 'man'
+    },
+    {
+        phonem: 'n',
+        word: 'rain'
+    },
+    {
+        phonem: 'ŋ',
+        word: 'king'
+    },
+    {
+        phonem: 'f',
+        word: 'fish'
+    },
+    {
+        phonem: 'v',
+        word: 'van'
+    },
+    {
+        phonem: 'θ',
+        word: 'thumb'
+    },
+    {
+        phonem: 'ð',
+        word: 'mother'
+    },
+    {
+        phonem: 's',
+        word: 'mouse'
+    },
+    {
+        phonem: 'z',
+        word: 'zebra'
+    },
+    {
+        phonem: 'ʃ',
+        word: 'shoe'
+    },
+    {
+        phonem: 'ʒ',
+        word: 'television'
+    },
+    {
+        phonem: 'h',
+        word: 'hand'
+    },
+    {
+        phonem: 'tʃ',
+        word: 'cheese'
+    },
+    {
+        phonem: 'dʒ',
+        word: 'jet'
+    },
+    {
+        phonem: 'r',
+        word: 'write'
+    },
+    {
+        phonem: 'l',
+        word: 'long'
+    },
+    {
+        phonem: 'j',
+        word: 'yo-yo'
+    },
+    {
+        phonem: 'w',
+        word: 'whale'
+    }
+]
+
+export const vowels = [
+    {
+        phonem: 'i:',
+        word: 'sea'
+    },
+    {
+        phonem: 'ɪ',
+        word: 'swim'
+    },
+    {
+        phonem: 'e',
+        word: 'bed'
+    },
+    {
+        phonem: 'æ',
+        word: 'cat'
+    },
+    {
+        phonem: 'ɑ:',
+        word: 'car'
+    },
+    {
+        phonem: 'ʌ',
+        word: 'cup'
+    },
+    {
+        phonem: 'ɒ',
+        word: 'lock'
+    },
+    {
+        phonem: 'ɔ:',
+        word: 'ball'
+    },
+    {
+        phonem: 'ʊ',
+        word: 'book'
+    },
+    {
+        phonem: 'u:',
+        word: 'two'
+    },
+    {
+        phonem: 'ɜ:ʳ',
+        word: 'shirt'
+    },
+    {
+        phonem: 'ə',
+        word: 'father'
+    }
+]
+
+export const diphthongs = [
+    {
+        phonem: 'eɪ',
+        word: 'eight'
+    },
+    {
+        phonem: 'aɪ',
+        word: 'eye'
+    },
+    {
+        phonem: 'ɔɪ',
+        word: 'boy'
+    },
+    {
+        phonem: 'oʊ',
+        word: 'arrow'
+    },
+    {
+        phonem: 'aʊ',
+        word: 'house'
+    },
+    {
+        phonem: 'eəʳ',
+        word: 'chair'
+    },
+    {
+        phonem: 'ɪəʳ',
+        word: 'deer'
+    },
+    {
+        phonem: 'ʊə',
+        word: 'tourist'
+    },
+]

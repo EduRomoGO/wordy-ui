3a6a55ff8ad65ea0ca3263e4bb0d00f01e87ee9f
EduRomoGO <eduromogo@outlook.es>
2020-02-09 00:24:33 +0000

    Updates

---
+++ precache-manifest.96b65930454c8dfd21fbc662a6b88790.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c4b1fd4ae653e5ec080000511bc16a2",
+    "url": "/wordy-ui/index.html"
+  },
+  {
+    "revision": "509b3ae60926df492b49",
+    "url": "/wordy-ui/static/css/main.005e04aa.chunk.css"
+  },
+  {
+    "revision": "5f8fd300631562d0b1ef",
+    "url": "/wordy-ui/static/js/2.fc05de3f.chunk.js"
+  },
+  {
+    "revision": "41cf9b16eb68a6867a468a93d99151df",
+    "url": "/wordy-ui/static/js/2.fc05de3f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "509b3ae60926df492b49",
+    "url": "/wordy-ui/static/js/main.ac85fff6.chunk.js"
+  },
+  {
+    "revision": "5b06107cc9b83a0ac2b4",
+    "url": "/wordy-ui/static/js/runtime-main.a5e03777.js"
+  },
+  {
+    "revision": "444e3404ea40f354d37b57e85dde17cf",
+    "url": "/wordy-ui/static/media/cancel.444e3404.svg"
+  }
+]);

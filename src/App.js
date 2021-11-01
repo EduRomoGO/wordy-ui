9db6a8fced945cd154c7d872356f274148e98027
EduRomoGO <eduromogo@outlook.es>
2021-11-01 15:09:05 +0100

    feat: adding home redirect

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import NavMenu from "./components/NavMenu/NavMenu.js";
 import Phonemes from "./components/Phonemes/Phonemes.js";
 import Spell from "./components/Spell/Spell.js";
 import { Words } from "./components/pages/words/Words";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
 function App() {
   return (
@@ -12,6 +17,7 @@ function App() {
       <div className="App fluid-type">
         <NavMenu />
 
+        <Redirect from="/" to="/words" />
         <Switch>
           <Route path="/words">
             <Words />

c99222b2f77a262b476324cf6ce6432b7b88b908
EduRomoGO <eduromogo@outlook.es>
2021-11-05 11:35:15 +0100

    refactor: clean

--- src/utils/db/load-all.js
+++ src/utils/db/load-all.js
@@ -5,13 +5,6 @@ const createOrOpenDb = () => {
   return db;
 };
 
-const createOrOpenPartsDb = () => {
-  const db = new window.PouchDB("parts");
-  console.log(`Opened connection to db ${db.name} using adapter ${db.adapter}`);
-
-  return db;
-};
-
 const checkDb = async (db) => {
   try {
     const dbInfo = await db.info();
@@ -34,14 +27,6 @@ const populate = (db, data) => {
     .catch(function (error) {
       console.log(`Error populating database - ${error}`);
     });
-};
-
-const getNextPendingPart = async (partsDb) => {
-  const pendingParts = await partsDb.find({
-    selector: { loaded: false },
-  });
-
-  return pendingParts.docs.length > 0 ? pendingParts.docs[0].name : undefined;
 };
 
 const writePendingPartWordsToDb = async (part, db) => {
@@ -69,48 +54,13 @@ const writePendingPartWordsToDb = async (part, db) => {
   }
 };
 
-const anyPendingPart = async (partsDb) => {
-  // const allDocs = await partsDb.allDocs({
-  //   include_docs: true,
-  // });
-
-  try {
-    const pendingParts = await partsDb.find({
-      selector: { loaded: false },
-    });
-
-    return pendingParts.docs.length > 0;
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-const getPendingParts = async (partsDb) => {
-  const pendingParts = await partsDb.find({
-    selector: { loaded: false },
-  });
-
-  return pendingParts.docs.length > 0
-    ? pendingParts.docs.map((n) => n.name)
-    : [];
-};
-
 export async function loadPendingParts(
   pendingParts = ["db-2.json", "db-3.json", "db-fail-4.json"]
 ) {
   try {
     const db = createOrOpenDb();
-    // const partsDb = createOrOpenPartsDb();
 
     const dbDocsCount = await checkDb(db);
-
-    // if (dbDocsCount < 3800) {
-    //   // await populate(partsDb, [
-    //   //   { _id: "db-2.json", name: "db-2.json", loaded: false },
-    //   //   { _id: "db-3.json", name: "db-3.json", loaded: false },
-    //   //   { _id: "db-4.json", name: "db-4.json", loaded: false },
-    //   // ]);
-    // }
 
     if (dbDocsCount < 3800) {
       const promList = pendingParts.map(async (pendingPart) => {
@@ -125,59 +75,11 @@ export async function loadPendingParts(
       return promListResults
         .filter((result) => result.status !== "fulfilled")
         .map((result) => result.reason);
-      // return promListResults
+    } else {
+      console.log("database is already populated");
+      return [];
     }
-    console.log("database is already populated");
-    return [];
-
-    // const pendingParts = await getPendingParts(partsDb);
-    // console.log("pendingParts");
-    // console.log(pendingParts);
   } catch (error) {
     console.error(`Error loading all database - ${error}`);
   }
 }
-
-// export async function loadPendingParts() {
-//   try {
-//     const db = createOrOpenDb();
-//     const partsDb = createOrOpenPartsDb();
-
-//     const dbDocsCount = await checkDb(db);
-
-//     if (dbDocsCount < 3800) {
-//       populate(partsDb, [
-//         { _id: "db-2.json", name: "db-2.json", loaded: false },
-//         { _id: "db-3.json", name: "db-3.json", loaded: false },
-//         { _id: "db-4.json", name: "db-4.json", loaded: false },
-//       ]);
-//     }
-
-//     // const partsDbDocsCount = await checkDb(partsDb);
-//     // console.log("dbDocsCount");
-//     // console.log(dbDocsCount);
-
-//     // mientras que haya alguno
-//     // coge el siguiente
-//     // escribe datas
-//     let isAnyPendingPart;
-
-//     do {
-//       isAnyPendingPart = await anyPendingPart(partsDb);
-//       if (isAnyPendingPart) {
-//         // const nextPendingPart = await getNextPendingPart(partsDb);
-//         getNextPendingPart(partsDb)
-//           .then((nextPendingPart) => {
-//             return writePendingPartWordsToDb(partsDb, nextPendingPart, db);
-//           })
-//           .then(() => {
-//             console.log("write");
-//           });
-
-//         // await writePendingPartWordsToDb(partsDb, nextPendingPart, db);
-//       }
-//     } while (isAnyPendingPart);
-//   } catch (error) {
-//     console.error(`Error loading all database - ${error}`);
-//   }
-// }

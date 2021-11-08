4266a3a0a3274c6e91f703a76c3babda1ef4d3a7
EduRomoGO <eduromogo@outlook.es>
2021-11-08 17:42:22 +0100

    refactor: simplify db creation

--- src/components/providers/DatabaseProvider.js
+++ src/components/providers/DatabaseProvider.js
@@ -3,7 +3,6 @@ import { jsx } from "@emotion/react";
 import styled from "@emotion/styled";
 import React, { useState, useEffect } from "react";
 import BounceLoader from "react-spinners/BounceLoader";
-import { checkDb } from "utils/db/db-helpers";
 
 const DatabaseContext = React.createContext();
 DatabaseContext.displayName = "DatabaseContext";
@@ -23,63 +22,49 @@ function useDatabaseContext() {
   return context;
 }
 
-const createOrOpenDb = async () => {
-  let db = new window.PouchDB("wordsDB");
-  await db.destroy();
-  db = new window.PouchDB("wordsDB");
-  console.log(`Opened connection to db ${db.name} using adapter ${db.adapter}`);
-
-  return db;
-};
-
-// TODO: Replace previous definition with this one
-// const createOrOpenDb = () => {
-//   const db = new window.PouchDB("wordsDB");
+// const createOrOpenDb = async () => {
+//   let db = new window.PouchDB("wordsDB");
+//   await db.destroy();
+//   db = new window.PouchDB("wordsDB");
 //   console.log(`Opened connection to db ${db.name} using adapter ${db.adapter}`);
 
 //   return db;
 // };
 
+// TODO: Replace previous definition with this one
+const createOrOpenDb = () => {
+  const db = new window.PouchDB("wordsDB");
+  console.log(`Opened connection to db ${db.name} using adapter ${db.adapter}`);
+
+  return db;
+};
+
 function DatabaseProvider({ children }) {
   const [db, setDb] = useState();
-  const [status, setStatus] = useState("idle");
 
   useEffect(() => {
-    const loadDatabase = async () => {
+    let db;
+    const createAndConfigurateDb = async () => {
       try {
-        // const db = createOrOpenDb();
-        const db = await createOrOpenDb();
+        db = createOrOpenDb();
+        // db = await createOrOpenDb();
+
+        await db.createIndex({
+          index: { fields: ["word"] },
+        });
+
         setDb(db);
-        setStatus("resolved");
       } catch (error) {
-        setStatus("rejected");
-        console.error(`Error loading database ${error}`);
+        throw new Error(`Imposible to connect to db - ${error}`);
       }
     };
 
-    setStatus("loading");
-    loadDatabase();
+    createAndConfigurateDb();
+
+    return () => {
+      db.close();
+    };
   }, []);
-
-  useEffect(() => {
-    if (db) {
-      const configDb = async () => {
-        const dbDocsCount = await checkDb(db);
-
-        if (dbDocsCount === 0) {
-          await db.createIndex({
-            index: { fields: ["word"] },
-          });
-        }
-      };
-
-      configDb();
-
-      return () => {
-        db.close();
-      };
-    }
-  }, [db]);
 
   const Frame = styled.div`
     height: 100vh;
@@ -89,18 +74,10 @@ function DatabaseProvider({ children }) {
     justify-content: center;
   `;
 
-  if (status === "loading" || status === "idle") {
+  if (!db) {
     return (
       <Frame>
         <BounceLoader color="#ff0000" loading={true} speedMultiplier={0.8} />
-      </Frame>
-    );
-  }
-
-  if (status === "rejected") {
-    return (
-      <Frame>
-        <div>There has been an error. Please try again later</div>
       </Frame>
     );
   }

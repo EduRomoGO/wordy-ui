524f45149feabd6a96d531e676ede25f397271bc
EduRomoGO <eduromogo@outlook.es>
2021-11-10 20:34:14 +0100

    feat: make spinner more generic

--- src/components/lib/Spinner.js
+++ src/components/lib/Spinner.js
@@ -2,11 +2,24 @@
 import { jsx } from "@emotion/react";
 import BounceLoader from "react-spinners/BounceLoader";
 import { Frame } from "components/lib";
+import { Spinner as RsSpinner } from "reactstrap";
 
-export const Spinner = () => {
-  return (
-    <Frame>
-      <BounceLoader color="#ff0000" loading={true} speedMultiplier={0.8} />
-    </Frame>
-  );
+export const Spinner = ({ size = "full" }) => {
+  if (size === "full") {
+    return (
+      <Frame>
+        <BounceLoader color="#ff0000" loading={true} speedMultiplier={0.8} />
+      </Frame>
+    );
+  }
+
+  if (size === "small") {
+    return (
+      <RsSpinner color="primary" size="sm">
+        Loading...
+      </RsSpinner>
+    );
+  }
+
+  throw new Error("Spinner size must be correctly specified");
 };

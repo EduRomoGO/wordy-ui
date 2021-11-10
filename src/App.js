6a4a44f0944f4e14b12f96745e6c33f775c26cfe
EduRomoGO <eduromogo@outlook.es>
2021-11-10 20:34:43 +0100

    feat: DatabaseLoadingStatus functionality and styles

--- src/App.js
+++ src/App.js
@@ -1,29 +1,79 @@
 /** @jsx jsx */
 import { jsx, css } from "@emotion/react";
-import { Suspense, lazy } from "react";
+import { Suspense, lazy, useState } from "react";
 import "./App.css";
 import NavMenu from "./components/NavMenu/NavMenu.js";
 import { Words } from "./components/pages/words/Words";
+import { ReactComponent as CancelIcon } from "SVG/cancel.svg";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import useLoadPendingParts from "hooks/useLoadPendingParts";
 import { useDatabaseLoadStatusContext } from "components/providers/DatabaseLoadStatusProvider";
+import { Spinner } from "components/lib";
 
 const Phonemes = lazy(() => import("./components/Phonemes/Phonemes.js"));
 const Spell = lazy(() => import("./components/Spell/Spell.js"));
 
 const DatabaseLoadingStatus = () => {
   const { loadStatus } = useDatabaseLoadStatusContext();
+  const [visible, setVisible] = useState(true);
 
-  return (
-    <div>
-      {loadStatus === "fullyLoaded" ? (
-        <div>Database is fully updated</div>
-      ) : (
-        <div>database loading in progress</div>
-      )}
-    </div>
-  );
+  const handleClearMessage = () => {
+    setVisible(false);
+  };
+
+  const isLoaded = loadStatus === "fullyLoaded";
+
+  const message = isLoaded
+    ? "Database is fully loaded! 🎉🎉"
+    : "Database loading in progress. Please be patient, this may take a while";
+
+  if (visible) {
+    return (
+      <div
+        css={css`
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          background-color: lightyellow;
+          border-bottom: 1px solid hsl(240deg 9% 69% / 10%);
+          position: relative;
+          padding: 2px 0;
+        `}
+      >
+        <p
+          css={css`
+            margin: 0;
+            margin-right: 8px;
+            font-size: 1rem;
+          `}
+        >
+          {message}
+        </p>
+        {!isLoaded && <Spinner size="small" />}
+        <div
+          css={css`
+            position: absolute;
+            right: 3px;
+            top: -3px;
+            cursor: pointer;
+          `}
+          onKeyPress={handleClearMessage}
+          onClick={handleClearMessage}
+        >
+          <CancelIcon
+            css={css`
+              width: 1rem;
+              vertical-align: unset;
+              fill: #807b7b;
+            `}
+          />
+        </div>
+      </div>
+    );
+  }
+
+  return null;
 };
 
 function App() {

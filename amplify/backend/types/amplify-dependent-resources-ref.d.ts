a236ae11430732c193658a46fff41fd0da31862d
Raquel <rqlromo@gmail.com>
2021-12-28 18:47:21 +0100

    add storage

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "wordyuid3781483": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "audiofiles": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}

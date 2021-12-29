d75443b98ae838287ca7b3114d1cd6a18dfbb1e5
Raquel <rqlromo@gmail.com>
2021-12-29 14:37:37 +0100

    add prettier

--- src/components/Phonemes/Phonemes.js
+++ src/components/Phonemes/Phonemes.js
@@ -1,27 +1,30 @@
 import React, { useEffect, useState, useRef } from "react";
 import "./Phonemes.css";
 import { consonants, vowels, diphthongs } from "./PhonemesList.js";
-import { Storage } from 'aws-amplify';
+import { Storage } from "aws-amplify";
 
-const PhonemList = ({items, type}) => {
-  return (<article className="phonemes-article">
-    <p className="title">{type}</p>
-    <div className={`phonemList ${type}`}>
-      {items.map(({ phonem, word }) => {
-        return <Phoneme key={`${phonem}-${word}`} phonem={phonem} word={word} />
-      })}
-    </div>
-  </article>
-)};
-
+const PhonemList = ({ items, type }) => {
+  return (
+    <article className="phonemes-article">
+      <p className="title">{type}</p>
+      <div className={`phonemList ${type}`}>
+        {items.map(({ phonem, word }) => {
+          return (
+            <Phoneme key={`${phonem}-${word}`} phonem={phonem} word={word} />
+          );
+        })}
+      </div>
+    </article>
+  );
+};
 
 const Phonemes = () => {
   return (
     <section className="phonemes-component-wrapper">
       <section className="phonemes-lists-wrapper">
-        <PhonemList items={consonants} type='consonants' />
-        <PhonemList items={vowels} type='vowels'  />
-        <PhonemList items={diphthongs} type='diphthongs'  />
+        <PhonemList items={consonants} type="consonants" />
+        <PhonemList items={vowels} type="vowels" />
+        <PhonemList items={diphthongs} type="diphthongs" />
       </section>
     </section>
   );
@@ -29,69 +32,60 @@ const Phonemes = () => {
 
 export default Phonemes;
 
-
-function Phoneme({phonem, word}) {
-  const [wordFile, setWordFile] = useState('')
-  const [phonemFile, setPhonemFile] = useState('')
-  const phonemeRef = useRef()
-  const wordRef = useRef()
+function Phoneme({ phonem, word }) {
+  const [wordFile, setWordFile] = useState("");
+  const [phonemFile, setPhonemFile] = useState("");
+  const phonemeRef = useRef();
+  const wordRef = useRef();
 
   const handlePhonemeClick = () => {
-    phonemeRef.current.play()
-  }
+    phonemeRef.current.play();
+  };
 
   const handleWordClick = () => {
-    wordRef.current.play()
-  }
+    wordRef.current.play();
+  };
 
   useEffect(() => {
     const getWord = async () => {
-        const file = await Storage.get(`phonemes/${word}.mp3`)
-        setWordFile(file)
-    }
+      const file = await Storage.get(`phonemes/${word}.mp3`);
+      setWordFile(file);
+    };
 
-    getWord()
+    getWord();
 
     const getPhonem = async () => {
-        const file = await Storage.get(`phonemes/${phonem}.mp3`)
-        setPhonemFile(file)
-    }
+      const file = await Storage.get(`phonemes/${phonem}.mp3`);
+      setPhonemFile(file);
+    };
 
-    getPhonem()
-
-  },[word, phonem])
+    getPhonem();
+  }, [word, phonem]);
 
   return (
     <div className="pair">
-      {phonemFile && (<>
-      <span
-        className="phonem"
-        onClick={handlePhonemeClick}
-      >
-        {phonem}
-      </span>
-      <audio ref={phonemeRef} hidden={true} controls>
-        <source
-          src={phonemFile}
-          type="audio/mpeg"
-        ></source>
-        Your browser does not support the audio element.
-      </audio>
-      </>)}
-      {wordFile && (<>      <span
-        className="phonem-word"
-        onClick={handleWordClick}
-      >
-        {word}
-      </span>
-      <audio ref={wordRef} hidden={true} controls>
-        <source
-          src={wordFile}
-          type="audio/mpeg"
-        ></source>
-        Your browser does not support the audio element.
-      </audio></>)}
-
+      {phonemFile && (
+        <>
+          <span className="phonem" onClick={handlePhonemeClick}>
+            {phonem}
+          </span>
+          <audio ref={phonemeRef} hidden={true} controls>
+            <source src={phonemFile} type="audio/mpeg"></source>
+            Your browser does not support the audio element.
+          </audio>
+        </>
+      )}
+      {wordFile && (
+        <>
+          <span className="phonem-word" onClick={handleWordClick}>
+            {word}
+          </span>
+          <audio ref={wordRef} hidden={true} controls>
+            <source src={wordFile} type="audio/mpeg"></source>
+            Your browser does not support the audio element.
+          </audio>
+        </>
+      )}
     </div>
   );
 }
